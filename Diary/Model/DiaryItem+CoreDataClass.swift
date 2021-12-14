2641e3926854f9006e1f1420c9956299ab774188
shunsuke250 <92722651+shunsuke250@users.noreply.github.com>
2021-12-14 11:17:35 +0900

    CoreData実装中

---
+++ Diary/Model/DiaryItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryItem+CoreDataClass.swift
+//  Diary
+//
+//  Created by 副山俊輔 on 2021/12/13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DiaryItem)
+public class DiaryItem: NSManagedObject {
+
+}
